13470013c26e9c4332e86ae61434dcfa802d16a6
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-13 14:18:31 +0530

    feat(core): integrate ngx-toastr for notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+//Angular Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Third-party
+import { ToastrModule } from 'ngx-toastr';
+//Custom Module
 import { AppRoutingModule } from './app-routing.module';
+//Components
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,8 +14,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    //Angular Modules
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    //Custom Module
+    AppRoutingModule,
+    //Third-party
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
