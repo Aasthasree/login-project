dbd20a5ded2e24bfbbfd882a2c03ba9f8f00df6a
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-15 12:30:08 +0530

    feat(login): initialize login form using FormBuilder

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+//Angular Imports
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(
+    private formBuilder : FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.initializeLoginForm();
+  }
+
+  initializeLoginForm(): void {
+   this.loginForm= this.formBuilder.group({
+      email: [''],
+      password: ['']
+    });
+  }
 
 }
