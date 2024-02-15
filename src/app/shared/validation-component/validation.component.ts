53325ceee88af0d05de1feeb6185d04e7f49a679
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-15 17:33:04 +0530

    feat(validation): add error message for invalid email

--- src/app/shared/validation-component/validation.component.ts
+++ src/app/shared/validation-component/validation.component.ts
@@ -6,8 +6,9 @@ import { Component, Input } from '@angular/core';
   template: `
   <ng-container *ngIf="control.invalid && (control.touched || control.dirty)" class="validation-error">
   <small *ngIf="control.errors?.required" class="mt">{{key}} is required</small>
-  <small *ngIf="control.errors?.pattern">{{key}} is invalid</small>
+  <small *ngIf="control.errors?.pattern">Invalid {{key}} format</small>
   <small *ngIf="control.errors?.cannotContainSpace">No spaces allowed in {{key}}</small>
+  <small *ngIf="control.errors?.email">Invalid {{key}} format</small>
  </ng-container>
 `,
   styles: ['small { color: red; }']
