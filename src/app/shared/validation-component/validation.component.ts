951169ea4e7cd952ccb40423800a75c0032b91b9
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-15 12:55:01 +0530

    feat(validation): implement validation error message display

--- src/app/shared/validation-component/validation.component.ts
+++ src/app/shared/validation-component/validation.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+//Angular Imports
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-validation',
-  template: '',
-  styles: []
+  template: `
+  <ng-container *ngIf="control.invalid && (control.touched || control.dirty)" class="validation-error">
+  <small *ngIf="control.errors?.required" class="mt">{{key}} is required</small>
+  <small *ngIf="control.errors?.pattern">{{key}} is invalid</small>
+ </ng-container>
+`,
+  styles: ['small { color: red; }']
 })
-export class ValidationComponent {
 
+export class ValidationComponent {
+  @Input() control: any = '';
+  @Input() key: string = '';
 }
