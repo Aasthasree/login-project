0699c1bad82bd10da3a715e96c0dc278346c09b3
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-19 14:18:14 +0530

    refactor(customer-module): organize imports in customer module

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,11 +1,20 @@
+//Angular Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+//Feature Module
 import { CustomerRoutingModule } from './customer-routing.module';
+//Components
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    LayoutComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     CustomerRoutingModule
