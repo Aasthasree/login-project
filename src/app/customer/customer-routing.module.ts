705519ba19f734a98d115ba58efd975fef8e3159
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-19 14:33:08 +0530

    feat(customer-routing): configure routes for layout and home component

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -1,7 +1,17 @@
+//Angular Imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Components
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LayoutComponent,
+children: [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
