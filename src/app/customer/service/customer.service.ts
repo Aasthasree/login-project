cbc216eac68c82c6f9a0a3a479cf7ae60b86a92c
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-19 18:28:53 +0530

    feat(customer-service): add customer service to fetch data

---
+++ src/app/customer/service/customer.service.ts
@@ -0,0 +1,44 @@
+//Angular Imports
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+//RXJS Imports
+import { Observable, catchError, throwError } from 'rxjs';
+//Environment
+import { environment } from 'src/environments/environment.development';
+//Model
+import { CustomerResponse } from '../customer-model/customer.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  private baseUrl: string = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  getCustomers(): Observable<CustomerResponse> {
+    const url = `${this.baseUrl}/api/e/customers/`;
+    return this.http.get<CustomerResponse>(url).pipe(
+      catchError(this.handleError.bind(this))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 404) {
+      console.error('Resource not found:', error);
+      this.router.navigate(['/admin/home']);
+      return throwError(() => 'The requested resource was not found.');
+    } else if (error.status === 500) {
+      console.error('Internal Server Error:', error);
+      return throwError(() => 'An internal server error occurred.');
+    } else {
+      console.error('API Error:', error);
+      return throwError(() => 'Something went wrong; please try again later.');
+    }
+  }
+}
