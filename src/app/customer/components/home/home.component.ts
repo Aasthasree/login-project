efc9e963cc23cc63f4942086cfa5157839c1cce8
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-19 18:31:10 +0530

    feat(home): update method to fetch customers from service

--- src/app/customer/components/home/home.component.ts
+++ src/app/customer/components/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+//Angular Imports
+import { Component, OnInit } from '@angular/core';
+//Model
+import { CustomerResponse } from '../../customer-model/customer.model';
+//Service
+import { CustomerService } from '../../service/customer.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  customers: CustomerResponse;
 
+  constructor(
+    private customerService: CustomerService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getCustomers();
+  }
+
+  private getCustomers() {
+    this.customerService.getCustomers().subscribe({
+      next: (response) => {
+        if (response) {
+          this.customers = response;
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+        alert(error);
+      }
+    });
+  }
 }
