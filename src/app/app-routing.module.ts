4d12d7ca40a43cf5b37f7db050cc8565cb921dc6
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-19 14:40:15 +0530

    feat(app-routing): add wildcard route for notfound page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //Components
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -11,6 +12,8 @@ const routes: Routes = [
     path: 'customer',
     loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),
   },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
