58beb85c538b385a72797a1760e527f8476f51ea
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-19 16:50:34 +0530

    feat(app-routing): add authentication guard to login and customer routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 //Components
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+//Guard
+import { authGuard } from './login/authguard/auth.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent },
+  {path: 'login', component: LoginComponent, canActivate: [authGuard] },
   {
     path: 'customer',
     loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),
+    canActivate: [authGuard]
   },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found' }
