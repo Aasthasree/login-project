5ee8cf6cf4eb4b7e2c4f7fbbb5437adf2072e537
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-20 16:26:39 +0530

    feat(customer-routing): add route for customer-detail component

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -4,13 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 //Components
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './components/home/home.component';
+import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent,
-children: [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-]}
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'home/:id', component: CustomerDetailComponent },
+    ]
+  }
 ];
 
 @NgModule({
